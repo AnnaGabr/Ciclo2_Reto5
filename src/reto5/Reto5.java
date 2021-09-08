2c7296e63d8237872080b05821fed3ebd085dd28
Anna <annagabriela2940@gmail.com>
2021-09-07 19:17:34 -0500

    Se termina de comentar, se pone el main de tal manera que al correr el archivo aparezca la ventana

--- src/reto5/Reto5.java
+++ src/reto5/Reto5.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package reto5;
 
-/**
- *
- * @author Gaby
- */
+import VistaControlador.VentanaPrincipal;
+
 public class Reto5 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new VentanaPrincipal().setVisible(true);
+            }
+        });
     }
     
 }
