8f8c2acd7ac2336c22e7cc501f8e03398ffdaf6f
Anna <annagabriela2940@gmail.com>
2021-09-07 20:04:04 -0500

    Se termino de comentar

--- src/reto5/Reto5.java
+++ src/reto5/Reto5.java
@@ -3,7 +3,8 @@ package reto5;
 import VistaControlador.VentanaPrincipal;
 
 public class Reto5 {
-
+    
+    // Nuestro main es que abra la ventana
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
--- src/utils/ConnectionDB.java
+++ src/utils/ConnectionDB.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package utils;
 
 import java.io.FileNotFoundException;
@@ -16,39 +11,46 @@ import java.util.logging.Logger;
 
 import org.json.simple.*;
 import org.json.simple.parser.*;
-/**
- *
- * @author Gaby
- */
+
 public class ConnectionDB {
     
+    // Vamos a definir el metodo por el cual nos conectamos a la base de datos
     public static Connection getConnection(){
+        // Iniciamos un lecto de JSON
         JSONParser parser = new JSONParser();
+        // Iniciamos una coneccion nula
         Connection conn = null;
         
         try {
+            // System.getProperty("user.dir") nos dara el path en donde se encuentra este proyecto,
+            // y lo concatenamos con el path en el que estan nuestras credenciales
             String credentials_path = System.getProperty("user.dir") + "/src/utils/credentials.json";
+            // Leemos las credenciales
             JSONObject jsonObject = (JSONObject)parser.parse(new FileReader(credentials_path));
             
+            // Sacamos el host (localhost), el puerto (3306), el usuario y la contraseña
             String host     = (String)jsonObject.get("db_ip");
             String port     = (String)jsonObject.get("dp_port");
             String username = (String)jsonObject.get("db_user");
             String password = (String)jsonObject.get("db_pssword");
+            // Escribimos la direccion de nuestra base de datos
             String dbURL = "jdbc:mysql://"+host+":"+port+"/reto4" ;
-            
+            // Y nos conectamos con Drivermanager
             conn = DriverManager.getConnection(dbURL, username, password);
+            // Revisamos si esta conectado
             if( conn != null ) 
-                System.out.println ( "Conectado" );
+                System.out.println ( "Conectado a la base de datos" );
             if( conn == null ) 
-                System.out.println ( "No conectado" );
+                System.out.println ( "No conectado a la base de datos" );
         }
+        // Si hay excepciones las imprimimos
         catch( SQLException | FileNotFoundException ex ) {
             ex.printStackTrace();
         } 
         catch (IOException | ParseException ex) {
             ex.printStackTrace();
         }
-        
+        // Retornamos esta coneccion
         return conn;
     }
     
